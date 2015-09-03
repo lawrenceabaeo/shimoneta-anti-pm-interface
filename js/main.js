69f4a5a6969fa630322b28950b6aa683349af434
Lawrence Abaeo <labaeo@gmail.com>
2015-09-02 21:35:08 -0700

    Fix #5. This is just the start of the functionality. More iteration are comming to get the UI closer to the one in the anime.

--- js/main.js
+++ js/main.js
@@ -1,85 +1,115 @@
 $(document).ready(function() {
-  /******************************/
+  /******************************************/
+  // Start of all the Countdown Timer stuff
   /** 
-      Countdown timer function modified 
-      from code found here:
+      Countdown timer function modified from:
       http://stackoverflow.com/a/20618517
-  ***/
+  **/
   function CountDownTimer(duration, numberOfMillisecondsToWaitBeforeExecutingCode) {
     this.duration = duration;
     this.numberOfMillisecondsToWaitBeforeExecutingCode = numberOfMillisecondsToWaitBeforeExecutingCode || 10; // Check every 10 milliseconds
     this.tickFuntions = []; // functions to run during each tick
     this.running = false;
+    this.lastDiff;
+    this.remainingTimeThatWasLeftWhenPaused;
   }
 
   CountDownTimer.prototype.start = function() {
     // console.log("At the top of the timer start method"); // Only called once
-    var start = Date.now(),
-        that = this,
-        diff, 
-        obj;
+    // console.log("this.lastDiff is: " + this.lastDiff);
+    var that = this,
+        startTime = Date.now(), 
+        objectOfTimeProperties;
     if (this.running) {
-      // console.log("It's running"); // This is never invoked?
+      // console.log("It's already running"); // This is never invoked?
       return;
     }
     this.running = true;
-
+    
+    
     /******************************/
-    (function timer() { // start of IIFE
+    // start of the IIFE
+    
+    (function timer() { 
       // console.log("in the timer IIFE");
+      // console.log("that.running is: " + that.running);
       
-      // How much time has already lapsed from when the timer started?
-      diff = that.duration - (((Date.now() - start)) | 0);
-      if (diff > 0) {
-        setTimeout(timer, that.numberOfMillisecondsToWaitBeforeExecutingCode); // run this timer IIFE again
-      } else {
-        diff = 0;
-        that.running = false;
-      }
-      
-      // Create an object that has properties for time remaining
-      objectOfTimeProperties = CountDownTimer.parseFromMilliseconds(diff);
-      
-      // Run functions that were passed into tickFunctions Array
-      // In this app, the formatAndDisplayer function is passed in
-      that.tickFuntions.forEach(function(ftn) {
-        ftn.call(this, objectOfTimeProperties.minutes, 
-                 objectOfTimeProperties.seconds, 
-                 objectOfTimeProperties.centiseconds);
-      }, that);
-    }()); // end of IIFE
-    /******************************/
-  };
+      if (that.running === true) {
+        
+        // How much time has already lapsed from when the timer started?
+        if (that.remainingTimeThatWasLeftWhenPaused === undefined) { // calculate remaining time using max duration
+          that.lastDiff = that.duration - (((Date.now() - startTime)) | 0);
+        } else { // calculate remaining time using the time that was remaining after the last pause
+          that.lastDiff = that.remainingTimeThatWasLeftWhenPaused - (((Date.now() - startTime)) | 0);
+        }
+        if (that.lastDiff > 0) {
+          setTimeout(timer, that.numberOfMillisecondsToWaitBeforeExecutingCode); // run this timer IIFE again
+        } else {
+          that.lastDiff = 0;
+          that.remainingTimeThatWasLeftWhenPaused = undefined;
+          that.running = false;
+        }
 
-  // Get any functions passed in and put it into tickFunctions Array,
-  // notice the IIFE function will run the tickFunctions Array
+        // Create an object that has properties of the time remaining
+        objectOfTimeProperties = CountDownTimer.parseFromMilliseconds(that.lastDiff);
+
+        // Run functions that were passed into tickFunctions Array
+        // In this app, the formatAndDisplayer function is passed in
+        that.tickFuntions.forEach(function(ftn) {
+          ftn.call(this, objectOfTimeProperties.minutes, 
+                   objectOfTimeProperties.seconds, 
+                   objectOfTimeProperties.centiseconds);
+        }, that);
+      } // end of if branch
+    }());
+    
+    // end of the IIFE
+    /******************************/
+    
+    
+  }; // end of CountDownTimer.prototype.start
+
   CountDownTimer.prototype.onTick = function(ftn) {
+    // Get functions passed into onTick and put it into tickFunctions Array
+    // ...notice the IIFE function will run the tickFunctions Array
     // console.log("In the onTick method");
     if (typeof ftn === 'function') {
       this.tickFuntions.push(ftn);
     }
     return this;
   };
-
-  CountDownTimer.prototype.expired = function() {
-    // Is the timer expired?
-    // return true if timer is no longer running
-    return !this.running;
-  };
-
-  // The format calculation is from here:
-  // http://stackoverflow.com/a/29553068/4771469
+  
   CountDownTimer.parseFromMilliseconds = function(milliseconds) {
+    // The format calculation is from here:
+    //   http://stackoverflow.com/a/29553068/4771469
     return {
       'minutes': (milliseconds / 60000) | 0,
       'seconds': ((milliseconds / 1000) % 60) | 0,
       'centiseconds': ((milliseconds / 10) % 100) | 0
     };
   };
-  /******************************/
+  
+  CountDownTimer.prototype.stop = function() {
+    this.running = false;
+    this.remainingTimeThatWasLeftWhenPaused = this.lastDiff;
+  };
+  
+  CountDownTimer.prototype.expired = function() {
+    // Is the timer expired?
+    // return true if timer is no longer running
+    return !this.running;
+  };
+  
+  // End of all the Countdown Timer stuff
+  /******************************************/
+  
 
   
-  var durationInSeconds = 5;
+  /******************************************/
+  // Start of the 'app' in action: 
+  
+  // 'app' variables and functions:
+  var durationInSeconds = 3*60;
   var timer = new CountDownTimer(durationInSeconds * 1000),
       objectThatHasTimeProps = CountDownTimer.parseFromMilliseconds(durationInSeconds * 1000);
   function formatAndDisplayer(minutes, seconds, centiseconds) {
@@ -90,22 +120,30 @@ $(document).ready(function() {
     $('#time').html(minutes + ':' + seconds + ':' + centiseconds);
   }
   
+  // 'app' party starts here:
   // Get and display the initial start/remaining time
   formatAndDisplayer(objectThatHasTimeProps.minutes, 
                      objectThatHasTimeProps.seconds, 
                      objectThatHasTimeProps.centiseconds);
   
   // Give the format/display function to the onTick method, 
-  // the onTick method will run this function whenever the 
-  // timer object's timer method is run:
+  // the onTick method then places that function in an Array,
+  // and the timer object's IIFE will run functions from that Array
   timer.onTick(formatAndDisplayer);
-
-  document.querySelector('button').addEventListener('click', 
+  
+  // Start listening:
+  document.querySelector('.start').addEventListener('click', 
     function () { 
       // console.log("About to trigger timer.start"); // Only called once
       timer.start(); 
   });
-    
+  document.querySelector('.stop').addEventListener('click', 
+    function () { 
+      timer.stop();
+  });
+  // End of the 'app' in action
+  /******************************************/
+  
 
 }); // end of $(document).ready(function() {
 
